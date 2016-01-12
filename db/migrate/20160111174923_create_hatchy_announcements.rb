d9b4958c8179f52a82ab88f793bfe361898af071
Hugo Marquez <hugomarquez.dev@gmail.com>
2016-01-11 19:43:13 -0600

    Check again

--- db/migrate/20160111174923_create_hatchy_announcements.rb
+++ db/migrate/20160111174923_create_hatchy_announcements.rb
@@ -1,7 +1,7 @@
 class CreateHatchyAnnouncements < ActiveRecord::Migration
   def change
     create_table :hatchy_announcements do |t|
-    	t.text				:message, 		null:false, limit: 80
+    	t.text 				:message, 		null:false, limit: 80
     	t.timestamp 	:starts_at, 	null: false
     	t.timestamp 	:ends_at,			null: false
     	t.timestamps
