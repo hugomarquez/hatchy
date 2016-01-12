ae6288d26184a42f65d4893507a9e68d1298e077
Hugo Marquez <hugomarquez.dev@gmail.com>
2016-01-11 19:50:09 -0600

    again :/

--- db/migrate/20160111175001_create_hatchy_bank_accounts.rb
+++ db/migrate/20160111175001_create_hatchy_bank_accounts.rb
@@ -1,10 +1,10 @@
 class CreateHatchyBankAccounts < ActiveRecord::Migration
   def change
     create_table :hatchy_bank_accounts do |t|
-    	t.belongs_to 	:user
-    	t.belongs_to 	:bank
-    	t.string 			:account, 				null: false
-    	t.string 			:account_digit, 	null: false
+      t.belongs_to  :user
+      t.belongs_to  :bank
+      t.string      :account,       null: false
+      t.string      :account_digit, null: false
       t.timestamps
     end
     add_index :hatchy_bank_accounts, [:user_id, :bank_id]
