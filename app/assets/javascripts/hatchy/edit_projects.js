f471da7fec2812da2197e3ea51b346121ac2d42b
Hugo Marquez <hugomarquez.dev@gmail.com>
2016-01-12 09:24:22 -0600

    Toggle hide and show functionality for sidebar and content wrappers

--- app/assets/javascripts/hatchy/edit_projects.js
+++ app/assets/javascripts/hatchy/edit_projects.js
@@ -2,14 +2,33 @@ var edit_projects = (function(){
   // Cache DOM
   var $rowSections = $('.container-fluid .hidden');
   var $currentRow = $('.container-fluid .hidden' + window.location.hash + '_dashboard');
+  var $toggleNav = $('.toggle-nav-collapse'); 
+  var $sidebarPage = $('.sidebar-page'); 
+  var $menuLinks = $('#project_menu li a');
   var $hashLink = $.grep( $( '#project_menu li a' ), function (o) {
     return o.hash === window.location.hash;
   });
+
+  // Bind Events
+  $toggleNav.on('click', toggleNavCss);
+  $menuLinks.not('.toggle-nav-collapse').on('click', toggleMenuActive);
   
   // Functions
   function showHashLocation() {
     $currentRow.removeClass('hidden');
-    $hashLink.parent().addClass('active');
+    $($hashLink).parent().addClass('active');
+  }
+
+  function toggleNavCss() {
+    $sidebarPage.toggleClass("collapsed expanded");
+    $('.toggle-nav-collapse i').toggleClass("fa-angle-right fa-angle-left");
+  }
+
+  function toggleMenuActive() {
+    $menuLinks.parent().removeClass('active');
+    $rowSections.addClass('hidden');
+    $(this).parent().addClass('active');
+    $('.container-fluid' + this.hash + '_dashboard').removeClass('hidden');
   }
 
   function initSummerNote() {
