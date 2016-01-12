2c90d2e2541dc3cad633302013b18354e9a110c5
Hugo Marquez <hugomarquez3x3@gmail.com>
2016-01-12 05:11:20 -0600

    remove hard tab

--- app/controllers/hatchy/announcements_controller.rb
+++ app/controllers/hatchy/announcements_controller.rb
@@ -1,12 +1,11 @@
 module Hatchy
   class AnnouncementsController < ApplicationController
-  	skip_before_filter :authenticate_user!, only: [:hide]
-  	
-  	def hide
-    	ids = [params[:id], *cookies.signed[:hidden_announcement_ids]]
-    	cookies.permanent.signed[:hidden_announcement_ids] = ids
-    	redirect_to :back
-  	end
-  	
+    skip_before_filter :authenticate_user!, only: [:hide]
+    
+    def hide
+      ids = [params[:id], *cookies.signed[:hidden_announcement_ids]]
+      cookies.permanent.signed[:hidden_announcement_ids] = ids
+      redirect_to :back
+    end    
   end
 end
