b68b537fb2bd81ba8ccfa15c0302c6be002f5929
Hugo Marquez <hugomarquez.dev@gmail.com>
2016-01-11 19:40:39 -0600

    check indentation

--- app/controllers/hatchy/application_controller.rb
+++ app/controllers/hatchy/application_controller.rb
@@ -1,7 +1,10 @@
 module Hatchy
   class ApplicationController < ActionController::Base
+    include Pundit
   	before_filter :authenticate_user!
   	protect_from_forgery
+
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   	private
   	def user_not_authorized
--- db/migrate/20160111175139_create_hatchy_projects.rb
+++ db/migrate/20160111175139_create_hatchy_projects.rb
@@ -1,24 +1,24 @@
 class CreateHatchyProjects < ActiveRecord::Migration
   def change
     create_table :hatchy_projects do |t|
-    	t.belongs_to    :user
-    	t.belongs_to    :category
-    	t.string        :project_img
-    	t.string        :city
-    	t.string 				:name, 								null: false
-    	t.string 				:headline
-    	t.string 				:about
-    	t.string 				:budget
-    	t.string 				:video_url
-    	t.string 				:short_url
-    	t.decimal	 			:goal,                precision: 8, scale: 2
-    	t.integer 	    :online_days
-    	t.timestamp			:online_date
-    	t.timestamp			:expires_at
-      t.timestamp     :rejected_at
-      t.timestamp     :send_to_draft_at
-    	t.string 				:status,							null: false
-    	t.boolean 	    :recommended,         null: false, default: false
+      t.belongs_to   :user
+      t.belongs_to   :category
+      t.string       :project_img
+      t.string       :city
+      t.string       :name,               null: false, limit: 50
+      t.string       :headline
+      t.string       :about
+      t.string       :budget
+      t.string       :video_url
+      t.string       :short_url
+      t.decimal      :goal,               precision: 8, scale: 2
+      t.integer      :online_days
+      t.timestamp    :online_date
+      t.timestamp    :expires_at
+      t.timestamp    :rejected_at
+      t.timestamp    :send_to_draft_at
+      t.string       :status,             null: false
+      t.boolean      :recommended,        null: false, default: false
       t.timestamps
     end
     add_index :hatchy_projects, :user_id
