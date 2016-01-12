554a838888abeb075eda6be9d27879a18de09e34
Hugo Marquez <hugomarquez.dev@gmail.com>
2016-01-11 19:56:18 -0600

    last one

--- db/migrate/20160111175124_create_hatchy_rewards.rb
+++ db/migrate/20160111175124_create_hatchy_rewards.rb
@@ -1,12 +1,12 @@
 class CreateHatchyRewards < ActiveRecord::Migration
   def change
     create_table :hatchy_rewards do |t|
-    	t.belongs_to 	:project
-    	t.decimal 		:min_value, 					null: false
-    	t.integer			:max_contributions
-    	t.string			:description, 				null: false
-    	t.integer			:row_order
-    	t.datetime		:deliver_at, 					null: false
+      t.belongs_to  :project
+      t.decimal     :min_value,         null: false
+      t.integer     :max_contributions
+      t.string      :description,       null: false
+      t.integer     :row_order
+      t.datetime    :deliver_at,        null: false
       t.timestamps
     end
     add_index :hatchy_rewards, :project_id
