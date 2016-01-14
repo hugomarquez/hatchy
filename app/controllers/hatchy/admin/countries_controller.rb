0cb865ac45be844bf911938ef8b810e743d01621
Hugo Marquez <hugomarquez3x3@gmail.com>
2016-01-14 16:11:08 -0600

    admin/countries controller and views

---
+++ app/controllers/hatchy/admin/countries_controller.rb
@@ -0,0 +1,61 @@
+module Hatchy
+  class Admin::CountriesController < Admin::ApplicationController
+    before_action :set_country, only: [:edit, :show, :update, :destroy]
+
+    # GET /countries
+    def index
+      @countries = Hatchy::Country.order(:name)
+    end
+
+    # GET /country/new
+    def new
+      @country = Hatchy::Country.new
+    end
+
+    # GET /country/:id/edit
+    def edit
+    end
+
+    # GET /country/:id
+    def show
+    end
+
+    # POST /country
+    def create
+      @country = Hatchy::Country.new(country_params)
+      if @country.valid?
+        @country.save
+        redirect_to admin_country_path(@country), notice: "Country saved successfully"
+      else
+        render :new
+      end
+    end
+    
+    # PATCH /country/:id
+    # PUT /country/:id
+    def update
+      if @country.update(country_params)
+        redirect_to admin_country_path(@country), notice: 'Country was successfully updated.'
+      else
+        render :edit
+        flash[:error] = @country.errors.full_messages.to_sentence
+      end
+    end
+
+    # DELETE /country/1
+    def destroy
+      @country.destroy
+      redirect_to admin_countries_path, notice: 'Country was successfully destroyed.'
+    end
+
+    private
+    def set_country
+      @country = Hatchy::Country.find(params[:id])
+    end
+
+    def country_params
+      params[:country].permit(:name, :code2, :code3, :continent, :tld, :currency, :eu_member)
+    end
+
+  end
+end
---
+++ spec/controllers/hatchy/admin/countries_controller_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+module Hatchy
+  RSpec.describe Admin::CountriesController, type: :controller do
+
+  end
+end
