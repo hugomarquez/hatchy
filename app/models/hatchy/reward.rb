bc6603ca89822cfd234a5756c860cb6bf56dadae
Hugo Marquez <hugomarquez3x3@gmail.com>
2016-01-18 17:10:30 -0600

    deliver_at no longer required field

--- app/models/hatchy/reward.rb
+++ app/models/hatchy/reward.rb
@@ -3,6 +3,6 @@ module Hatchy
     belongs_to :project,      class_name:'Hatchy::Project'
     has_many  :contributions, class_name:'Hatchy::Contribution', dependent: :nullify
     has_many  :payments,      through: :contributions
-    validates_presence_of :min_value, :description, :deliver_at
+    validates_presence_of :min_value, :description
   end
 end
