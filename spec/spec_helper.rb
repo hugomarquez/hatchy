190aab892e630edfc4ed7ff28ed058fe06108a14
Hugo Marquez <hugomarquez3x3@gmail.com>
2016-01-18 19:21:20 -0600

    added coveralls.wear!

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,11 @@
 require 'simplecov'
 require 'coveralls'
+
 SimpleCov.start do 
 	minimum_coverage(80.0)
 end
+
+Coveralls.wear!
 
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
