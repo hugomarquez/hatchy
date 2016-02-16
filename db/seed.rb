b7effd0f287b3f51dcb7441f3ed55d51b356c3c6
Hugo Marquez <hugomarquez3x3@gmail.com>
2016-02-15 21:31:21 -0600

    Reduced seed for now

--- db/seed.rb
+++ db/seed.rb
@@ -1,43 +1,16 @@
 puts 'Seeding Database...'
 [
-  { en: 'Art'},
-  { en: 'Visual Arts'},
-  { en: 'Circus' },
-  { en: 'Community' },
-  { en: 'Humor' },
-  { en: 'Comicbooks' },
-  { en: 'Dance' },
-  { en: 'Design' },
-  { en: 'Events' },
-  { en: 'Fashion' },
-  { en: 'Gastronomy' },
-  { en: 'Film & Video' },
-  { en: 'Games' },
-  { en: 'Journalism' },
-  { en: 'Music' },
-  { en: 'Photography' },
-  { en: 'Science & Technology' },
-  { en: 'Theatre' },
-  { en: 'Sport' },
   { en: 'Web' },
-  { en: 'Carnival' },
-  { en: 'Architecture & Urbanism' },
-  { en: 'Literature' },
-  { en: 'Mobility & Transportation' },
-  { en: 'Environment' },
   { en: 'Social Business' },
   { en: 'Education' },
-  { en: 'Fiction Films' },
-  { en: 'Documentary Films' },
-  { en: 'Experimental Films' },
   { en: 'Health'}
 ].each do |name|
    category = Hatchy::Category.find_or_initialize_by(name: name[:en ])
    category.save
  end
 
-bank = Hatchy::Bank.find_or_initialize_by(name:"Fake Bank", code:"69")
+bank = Hatchy::Bank.find_or_initialize_by(name:"Fake Bank", code:"1")
 bank.save
 
-bank2 = Hatchy::Bank.find_or_initialize_by(name:"Fake Second Bank", code:"96")
+bank2 = Hatchy::Bank.find_or_initialize_by(name:"Fake Second Bank", code:"2")
 bank2.save
