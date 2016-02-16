ee688007a0a997c6ebbf35bfee811fe22204dedd
Hugo Marquez <hugomarquez3x3@gmail.com>
2016-02-15 21:32:41 -0600

    replace query with scope

--- app/controllers/hatchy/users/contributions_controller.rb
+++ app/controllers/hatchy/users/contributions_controller.rb
@@ -1,12 +1,12 @@
 module Hatchy
   class Users::ContributionsController < ApplicationController
-  	before_action :set_project, only:[:new, :edit, :create, :update]
+    before_action :set_project, only:[:new, :edit, :create, :update]
     before_action :set_contribution, only:[:edit, :update]
 
-  	def index
-  		@active = Hatchy::Contribution.where(user: current_user).joins(:project).where('hatchy_projects.status': 'online')
-  		@collected = Hatchy::Contribution.where(user: current_user).joins(:project).where('hatchy_projects.status': 'successful')
-  	end
+    def index
+      @active = Hatchy::Contribution.active_pledges(current_user)
+      @collected = Hatchy::Contribution.collected_pledges(current_user)
+    end
 
   end
 end
